bc30409563a106d8cede93fbae2d2371893327b5
Ting-Yuan Hsia <ly2314@gmail.com>
2015-10-16 00:53:02 +0800

    update yandex parsing rules

--- yandex.php
+++ yandex.php
@@ -19,21 +19,21 @@
     
     $dom = new DOMDocument;
     $dom->loadHTML($html);
-    $metas = $dom->getElementsByTagName('meta');
+    $spans = $dom->getElementsByTagName('span');
     $author = 'test';
     $description = 'test';
-    foreach ($metas as $meta)
+    foreach ($spans as $span)
     {
-        if ($meta->getAttribute('property') == 'og:title')
+        if ($span->getAttribute('class') == 'b-501px__name')
         {
-            $author = $meta->getAttribute('content');
+            $author = $span->textContent;
             break;
         }
     }
     $ps = $dom->getElementsByTagName('p');
     foreach ($ps as $p)
     {
-        if ($p->getAttribute('class') == 'b-500px__description')
+        if ($p->getAttribute('class') == 'b-501px__description')
         {
             $description = $p->textContent;
             break;
@@ -42,9 +42,11 @@
     
     $message = $img_url."\r\nYandex每日桌布\r\n**".$author."**\r\n#Yandex每日桌布";
     $qulifier = 'shares';
+    print_r($message);
     $post_info = do_action("http://www.plurk.com/APP/Timeline/plurkAdd", array("content" => rawurlencode($message), "qualifier" => $qulifier, "lang" => 'tr_ch'));
     print_r($post_info);    
     
     $plurk_id = $post_info['plurk_id'];
+    print_r($description);
     $post_info = do_action("http://www.plurk.com/APP/Responses/responseAdd", array("content" => rawurlencode($description), "qualifier" => $qulifier, "plurk_id" => $plurk_id, "lang" => 'tr_ch'));
 ?>
